e7dd7fb556ba411a3ada5584a7ca63d9a47a6b76
dburns98 <dylanburns698@gmail.com>
2022-07-25 23:36:28 -0400

    fixed issues with generatePassword function

--- script.js
+++ script.js
@@ -4,36 +4,55 @@ const characters = {
   lower: "abcdefghijklmnopqrstuvwxyz",
   symbols: " !#$%&'*+,-./:;<=>?@^_`|~",
   numbers: "0123456789",
-  
+
 };
+
+  
+
 
 // function for generating password 
 function generatePassword() {
+  
+  
+
   var charset = "";
+
+// asks user how many characters they want in the password
   var length = window.prompt("How long would you like the password to be? (Keep it between 8 and 128.) ");
-  var upperCase = window.confirm("Would you like to include Upper case letters in the password?");
-  if(upperCase) {
+  
+// asks the user if they waNT TO INCLude uppercase letters
+  var upperCase = window.prompt("Would you like to include Upper case letters in the password? Type yes or no.");
+  if(upperCase = "yes") {
     charset += characters.upper;
   };
   
-  var lowerCase = window.confirm("Would you also like to include lower case letters in the password?");
-  if(lowerCase) {
+//asks if they want to include lowercase
+  var lowerCase = window.prompt("Would you also like to include lower case letters in the password? Type yes or no.");
+  if(lowerCase = "yes") {
     charset += characters.lower;
   };
 
-  var number = window.confirm("Would you also like to include Numbers in the password?");
-  if(number) {
+// asks if they want to include numbers
+  var number = window.prompt("Would you also like to include Numbers in the password? Type yes or no");
+  if(number = "yes") {
     charset += characters.numbers;
   };
 
-  var symbol = window.confirm("Would you alkso like to include symbols in the password?");
-  if(symbol) {
+//asks if they want to include symbols
+  var symbol = window.prompt("Would you alkso like to include symbols in the password? Type yes or no .");
+  if(symbol = "yes") {
     charset += characters.symbols;
-  };
-};
+  }
+
+  for(let i=0; i < length; i++) {
+    var randomPass = Math.floor(Math.random() * charset)
+ 
+    return randomPass
+
+ };
   
 
-
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -48,4 +67,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
